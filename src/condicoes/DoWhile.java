ed22017fcb4e10d66b43760bdd280a129af00121
Jefferson Alves <jefff9910@outlook.com>
2022-02-20 22:36:07 -0300

    For

--- src/condicoes/DoWhile.java
+++ src/condicoes/DoWhile.java
@@ -1,7 +1,31 @@
 package condicoes;
 
+import java.util.Scanner;
+
 public class DoWhile {
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+//		String entrada = "";
+//		
+//		do {
+//			System.out.println("Diga algo: ");
+//			entrada = teclado.nextLine();
+//			
+//			System.out.println(entrada);
+//				
+//			
+//		} while(!entrada.equalsIgnoreCase("sair"));
+//		
+//		
+//		System.out.print("Obrigado!!!");
+//		
+//		
+//		teclado.close();
+
+		int i = 3;
+		System.out.println(10 - 5 * 2 + --i);
 			
+		
 	}
 }
---
+++ src/condicoes/For2.java
@@ -0,0 +1,14 @@
+package condicoes;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 10; i >= 0; i -= 2) {
+			System.out.printf("Contador: %d \n", i);
+		}
+		
+	}
+
+}
