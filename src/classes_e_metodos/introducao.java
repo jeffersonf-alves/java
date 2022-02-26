0db146c726727d5155f35fdfe4a6b5ff860b63df
Jefferson Alves <jefff9910@outlook.com>
2022-02-25 23:04:48 -0300

    Introducao a metodos

---
+++ src/classes_e_metodos/introducao.java
@@ -0,0 +1,27 @@
+package classes_e_metodos;
+
+public class introducao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(setName());
+	}
+	public static String setName() {
+		return "Jefferson";
+	}
+	
+	// Void não tem retorno
+	public void getIdade() {
+		
+	}
+
+	public double getJuros() {
+		return 2.5d;
+	}
+	
+	public int getAno() {
+		return 2022;
+	}
+	
+	
+}
