cb677f557f9b84ae0e786fcf276e7aee325faa5b
Jefferson Alves <jefff9910@outlook.com>
2022-07-02 23:35:38 -0300

    funcoes

--- src/Funcoes/Teclado.java
+++ src/Funcoes/Teclado.java
@@ -6,14 +6,35 @@ public class Teclado {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		
-		System.out.printf("Valores digitados: "+a+" "+b+" "+c);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		int c = sc.nextInt();
+//		
+//		System.out.printf("Valores digitados: "+a+" "+b+" "+c);
 
+		int maiorNumero = max(5, 4, 10);
+		showResult(maiorNumero);
+		
+	}
+	
+	public static int max(int x, int y,int z) {
+		
+		int aux;
+		if(x > y && x > z) {
+			aux = x;
+		} else if(y > z) {
+			aux = y;
+		} else {
+			aux = z;
+		}
+		
+		return aux;
+	}
+	
+	public static void showResult(int value) {
+		System.out.print(value);
 	}
 
 }
